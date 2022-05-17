7b9073213e2ebb5c99257be01c5f45212e5d206d
Rafael <rafaelvlfarias@gmail.com>
2022-05-16 22:50:53 -0300

    add mensagem e link ao email de recuperação de senha

--- src/main/java/com/incluwed/incluwed/messages/ForgotPasswordMessage.java
+++ src/main/java/com/incluwed/incluwed/messages/ForgotPasswordMessage.java
@@ -1,4 +1,41 @@
 package com.incluwed.incluwed.messages;
 
+import com.incluwed.incluwed.infrastructure.auth.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+
 public class ForgotPasswordMessage {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public void sendEmail(String recipientEmail, String link)
+            throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+
+        helper.setFrom("incluwebcontato@gmail.com", "Support Incluweb");
+        helper.setTo(recipientEmail);
+        helper.setSubject("Redifinição de senha - Incluweb");
+
+        String content = "<p>Hello,</p>"
+                + "<p>You have requested to reset your password.</p>"
+                + "<p>Click the link below to change your password:</p>"
+                + "<p><a href=\"" + "http://localhost" + "\">Change my password</a></p>"
+                + "<br>"
+                + "<p>Ignore this email if you do remember your password, "
+                + "or you have not made the request.</p>";
+
+        helper.setText(content, true);
+        javaMailSender.send(message);
+    }
 }
